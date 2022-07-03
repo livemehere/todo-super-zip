f5477563d65725b7450f975ce8807943ec7be590
livemehere <rhdxoals@gmail.com>
2022-07-04 05:53:59 +0900

    page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Home = React.lazy(() => import("./pages/home"));
+const Detail = React.lazy(() => import("./pages/detail"));
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
